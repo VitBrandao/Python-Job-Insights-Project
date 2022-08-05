54b6b16ac5f6e327aeea98f959f8b2b2ec3c4d67
vitor <vitorsbsilva@gmail.com>
2022-08-05 13:56:39 -0300

    test: (req.10) test_counter. Add six test cases to count_ocurrences function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,27 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    'Deve retornar a quantidade exata de ocorrências da palavra JavaScript'
+    function_call = count_ocurrences("src/jobs.csv", "JavaScript")
+    assert function_call == 122
+
+    'Deve retornar a quantidade exata de ocorrências da palavra Python'
+    function_call = count_ocurrences("src/jobs.csv", "Python")
+    assert function_call == 1639
+
+    'Não deve retornar a quantidade quando digitado incorretamente JavaScript'
+    function_call = count_ocurrences("src/jobs.csv", "JavasScript")
+    assert function_call != 122
+
+    'Não deve retornar a quantidade quando digitado incorretamente Python'
+    function_call = count_ocurrences("src/jobs.csv", "Pyton")
+    assert function_call != 1639
+
+    'Não deve diferenciar maiúsculas e minúsculas para JavaScript'
+    function_call = count_ocurrences("src/jobs.csv", "javAsCriPt")
+    assert function_call == 122
+
+    'Não deve diferenciar maiúsculas e minúsculas para Python'
+    function_call = count_ocurrences("src/jobs.csv", "pYtHOn")
+    assert function_call == 1639
